ccedcab65c7f32bc4260e75042b35f36e840f451
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2024-11-08 12:02:43 -0300

    Add sidebar com filtros na page dashboards.

--- Dashboard.py
+++ Dashboard.py
@@ -16,9 +16,32 @@ def  format_number(value, prefix=""):
 st.title("DASHBOARD DE VENDAS :shopping_trolley:")
 
 url = "https://labdados.com/produtos"
-response = requests.get(url)
+regioes = ["Brasil", "Centro-Oeste", "Nordeste", "Norte", "Sudeste", "Sul"]
+
+st.sidebar.title("Filtros")
+regiao = st.sidebar.selectbox("Região", regioes)
+
+if regiao == "Brasil":
+    regiao = ""
+
+todos_anos = st.sidebar.checkbox("Dados de todo o período", value=True)
+if todos_anos:
+    ano = ""
+else:
+    ano = st.sidebar.slider("Ano", 2020, 2023)
+
+query_string = {
+    "regiao": regiao.lower(),
+    "ano": ano
+}
+
+response = requests.get(url, params=query_string)
 dados = pd.DataFrame.from_dict(response.json())
 dados["Data da Compra"] = pd.to_datetime(dados["Data da Compra"], format="%d/%m/%Y")
+
+filtro_vendedores = st.sidebar.multiselect("Vendedores", dados["Vendedor"].unique())
+if filtro_vendedores:
+    dados = dados[dados["Vendedor"].isin(filtro_vendedores)]
 
 ## Tabelas
 
@@ -193,8 +216,6 @@ with aba2:
         st.metric("Quantidade de Vendas", format_number(dados.shape[0]))
         st.plotly_chart(fig_venda_mensal, use_container_width=True)
         st.plotly_chart(fig_venda_categorias, use_container_width=True)
-        
-    st.dataframe(dados)
 
 # Vendedores
 with aba3:
